0ce22d44f43aa8ad4f4ee23ca3562caeb1046be1
Quang <minhquangvl2005@gmail.com>
2025-09-10 12:20:31 +0700

    first commit

---
+++ Areas/Client/Controllers/ClientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventSphere.Areas.Client.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
