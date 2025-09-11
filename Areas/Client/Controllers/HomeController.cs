e30f176aab6884395b02b0dd7523c556293bc296
0868397300 <trongnguyenphu2509@gmail.com>
2025-09-11 16:18:00 +0700

    clientHome lan 1

---
+++ Areas/Client/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using EventSphere.Models.Repositories;
+using EventSphere.Models.ModelViews;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventSphere.Areas.Client.Controllers
+{
+    [Area("Client")]
+    public class HomeController : Controller
+    {
+        private readonly HomeRepository _repo;
+
+        public HomeController(HomeRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var vm = new HomeViewModel
+            {
+                UpcomingEvents = await _repo.GetUpcomingEventsAsync(),
+                LatestMedia = await _repo.GetLatestMediaAsync()
+            };
+
+            ViewData["Title"] = "Trang chủ";
+            return View(vm);
+        }
+    }
+}
